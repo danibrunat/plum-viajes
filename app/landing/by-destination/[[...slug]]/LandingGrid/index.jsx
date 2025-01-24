69a204f0fa6b4f6411dc8c6a39d2ebfee5f36c77
Dani Brunat <danii.rhcp@gmail.com>
2025-01-24 11:04:57 -0300

    add key

--- app/landing/by-destination/[[...slug]]/LandingGrid/index.jsx
+++ app/landing/by-destination/[[...slug]]/LandingGrid/index.jsx
@@ -5,7 +5,7 @@ export default function LandingGrid({ product, landingData }) {
   return (
     <div className="grid gap-2 grid-cols-2 md:gap-3 md:grid-cols-6 mb-12 p-2">
       {landingData.map((destination) => (
-        <div className="p-2">
+        <div className="p-2" key={Math.random()}>
           <LandingGridItem product={product} destination={destination} />
         </div>
       ))}
