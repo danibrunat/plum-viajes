9852ddaeae36e8c9d65e95547846672d7aac8505
Dani Brunat <danii.rhcp@gmail.com>
2025-03-31 16:23:29 -0300

    das

--- next.config.js
+++ next.config.js
@@ -35,19 +35,6 @@ const nextConfig = {
       fullUrl: true,
     },
   },
-  experimental: {
-    turbo: {
-      resolveExtensions: [
-        ".mdx",
-        ".tsx",
-        ".ts",
-        ".jsx",
-        ".js",
-        ".mjs",
-        ".json",
-      ],
-    },
-  },
   async headers() {
     return [
       {
