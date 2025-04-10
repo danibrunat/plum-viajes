21250b35a3f1b77d27fcb614e33a581985186607
Dani Brunat <danii.rhcp@gmail.com>
2025-04-10 17:52:37 -0300

    das

--- app/api/lib/redis.js
+++ app/api/lib/redis.js
@@ -1,13 +1,8 @@
 import { Redis } from "@upstash/redis";
-const isFrontEndCall = typeof process.env.SANITY_STUDIO_URL === "undefined";
 
 const redis = new Redis({
-  url: isFrontEndCall
-    ? process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL
-    : process.env.SANITY_STUDIO_UPSTASH_REDIS_REST_URL,
-  token: isFrontEndCall
-    ? process.env.UPSTASH_REDIS_REST_TOKEN
-    : process.env.SANITY_STUDIO_UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.SANITY_STUDIO_UPSTASH_REDIS_REST_URL,
+  token: process.env.SANITY_STUDIO_UPSTASH_REDIS_REST_TOKEN,
 });
 
 export default redis;
