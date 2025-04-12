0dc20389f5929089051b8576d0d2b3e0e9169bbd
Dani Brunat <danii.rhcp@gmail.com>
2025-04-12 10:58:24 -0300

    das

--- utils/sanity-utils.ts
+++
@@ -1,8 +0,0 @@
-import clientConfig from "./sanity-client-config";
-import imageUrlBuilder from "@sanity/image-url";
-
-export const builder = imageUrlBuilder(clientConfig);
-
-export function urlFor(source: any) {
-  return builder.image(source);
-}
