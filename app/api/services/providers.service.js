3043b077b7d701f10d55e32fc85cbeed0dce07f7
Dani Brunat <danii.rhcp@gmail.com>
2025-04-13 12:12:51 -0300

    das

--- app/api/services/providers.service.js
+++ app/api/services/providers.service.js
@@ -728,7 +728,10 @@ export const ProviderService = {
     if (!monthYear) return null;
 
     const [month, year] = monthYear.split("-");
-    const today = new Date(); // Obtener la fecha de hoy
+    // Fecha de hoy + 1 día.
+
+    const today = new Date();
+    today.setDate(today.getDate() + 1); // Sumar un día a la fecha de hoy
     const currentMonthLastDay = new Date(year, month, 0).getDate();
 
     // Verificar si el mes y año proporcionados coinciden con el mes y año actual
