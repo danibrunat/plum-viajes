e4b6ca34341cb48cd12f8db3e411f82efcc90918
Dani Brunat <danii.rhcp@gmail.com>
2025-04-17 09:39:00 -0300

    das

---
+++ app/robots.js
@@ -0,0 +1,36 @@
+import { groq } from "next-sanity";
+import { client } from "./lib/client";
+
+export default async function robots() {
+  // Fetch all routes that have disallowRobots set to true
+  const { disallowedRoutes } = await client.fetch(groq`{
+    "disallowedRoutes": *[
+      _type == "route" &&
+      !(_id in path("drafts.**")) &&
+      disallowRobots == true
+    ]{
+      "slug": slug.current
+    }
+  }`);
+
+  // Create disallow rules for routes that should be hidden from robots
+  const disallowRules =
+    disallowedRoutes?.map((route) => ({
+      userAgent: "*",
+      disallow: `/${route.slug === "/" ? "" : route.slug}`,
+    })) || [];
+
+  // Add default allow rule
+  const rules = [
+    {
+      userAgent: "*",
+      allow: "/",
+    },
+    ...disallowRules,
+  ];
+
+  return {
+    rules,
+    sitemap: `${process.env.URL || "http://localhost:3000"}/sitemap.xml`,
+  };
+}
