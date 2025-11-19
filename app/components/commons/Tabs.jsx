6af40a828b0e042cafee6dcbf0a1827dc8a548f1
Dani Brunat <danii.rhcp@gmail.com>
2025-11-19 20:51:21 -0300

    label undefined

--- app/components/commons/Tabs.jsx
+++ app/components/commons/Tabs.jsx
@@ -1,7 +1,11 @@
 "use client";
 import React, { useState } from "react";
 const Tabs = ({ children }) => {
-  const [activeTab, setActiveTab] = useState(children[0]?.props.label);
+  const tabChildren = React.Children.toArray(children).filter(
+    (child) => React.isValidElement(child) && child.props?.label
+  );
+
+  const [activeTab, setActiveTab] = useState(tabChildren[0]?.props.label);
   const handleClick = (e, newActiveTab) => {
     e.preventDefault();
     if (newActiveTab === "Hoteles") {
@@ -14,25 +18,25 @@ const Tabs = ({ children }) => {
   return (
     <>
       <div className="flex">
-        {children?.map((child) => (
+        {tabChildren.map((child) => (
           <button
-            key={child.props.label}
-            aria-label={child.props.label}
+            key={child?.props?.label}
+            aria-label={child?.props?.label}
             className={`${
-              activeTab === child.props.label
+              activeTab === child?.props?.label
                 ? " bg-plumPrimaryPurple text-white"
                 : ""
             } flex-1 text-gray-700 font-normal md:font-medium py-2 `}
-            onClick={(e) => handleClick(e, child.props.label)}
+            onClick={(e) => handleClick(e, child?.props?.label)}
           >
-            {child.props.label}
+            {child?.props?.label}
           </button>
         ))}
       </div>
       <div>
-        {children?.map((child) => {
-          if (child.props.label === activeTab) {
-            return <div key={child.props.label}>{child.props.children}</div>;
+        {tabChildren.map((child) => {
+          if (child?.props?.label === activeTab) {
+            return <div key={child?.props?.label}>{child?.props.children}</div>;
           }
           return null;
         })}
