c0fb520f991519b680d619fd3351b012b0e8cca0
Dani Brunat <danii.rhcp@gmail.com>
2025-11-28 16:20:17 -0300

    icons

--- app/packages/detail/_components/PkgDetail/Departures/Form/index.jsx
+++ app/packages/detail/_components/PkgDetail/Departures/Form/index.jsx
@@ -185,13 +185,7 @@ const DeparturesForm = ({ departures = [] }) => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M8 7V3a4 4 0 118 0v4c0 3-2 5-4 5s-4-2-4-5z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M3 21v-6a2 2 0 012-2h14a2 2 0 012 2v6"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             />
           </svg>
           <span className="text-plumPrimaryPurple font-medium text-sm">
@@ -225,7 +219,7 @@ const DeparturesForm = ({ departures = [] }) => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M8 7V3a4 4 0 118 0v4c0 3-2 5-4 5s-4-2-4-5z"
+                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                   />
                 </svg>
                 Fecha de Salida
@@ -317,7 +311,7 @@ const DeparturesForm = ({ departures = [] }) => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M14.828 14.828a4 4 0 01-5.656 0M9 10h1.5a2.5 2.5 0 100-5H9v5z"
+                    d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
                   />
                 </svg>
                 Menores
